88837504e901309aa71523b9938ac08308276d8c
Ankit Rai <161692587+ankitrai2304@users.noreply.github.com>
2025-07-25 16:34:10 +0530

    major components for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import Testimonials from "./sections/Testimonials";
+import Footer from "./sections/Footer";
+import Contact from "./sections/Contact";
+import TechStack from "./sections/TechStack";
+import Experience from "./sections/Experience";
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import LogoShowcase from "./sections/LogoShowcase";
+import FeatureCards from "./sections/FeatureCards";
+import Navbar from "./components/NavBar";
 
-const App = () => {
-    return (
-<main>
-    <div>
-        <h1>
-            welcome to my 3d portfolio!
-        </h1>
-    </div>
-</main>
-    )
-}
+const App = () => (
+    <>
+        <Navbar />
+        <Hero />
+        <ShowcaseSection />
+        <LogoShowcase />
+        <FeatureCards />
+        <Experience />
+        <TechStack />
+        <Testimonials />
+        <Contact />
+        <Footer />
+    </>
+);
 
-export default App
+export default App;
